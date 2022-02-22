aae763ab7d4b1788ee2af197c606faec5e041205
Björn Strömberg <bjorn.stromberg@codic.se>
2022-02-22 08:59:52 +0100

    [feat] excercise 1 facit

--- AsyncAwait-Callbacks/facit.js
+++ AsyncAwait-Callbacks/facit.js
@@ -2,7 +2,17 @@
  ** Exercise 1 **
 \****************/
 (async function () {
-  //...
+  const url = new URL("http://localhost:5176");
+  url.pathname = "api/image/1";
+
+  const image = await fetch(url).then((res) => res.json());
+
+  const imgElement = document.querySelector("#ex-1 img");
+  const pElement = document.querySelector("#ex-1 p");
+
+  imgElement.src = image.url;
+  imgElement.width = image.metadata.width / 2;
+  pElement.textContent = image.metadata.size;
 })();
 
 /****************\
